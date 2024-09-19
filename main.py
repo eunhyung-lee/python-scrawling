d4bbba49da673b4d77ad1b6872d8d63fb4570143
eunhyung-lee <dldnsgud12@naver.com>
2024-09-19 21:15:53 +0900

    #6.5 Pagination

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
+all_jobs = []
 for job in jobs:
     title = job.find("span", class_="title")
     company, position, region = job.find_all("span", class_="company")
-    company = company.text
-    position = position.text
-    region = region.text
-    print(f"{company}  {position}  {region}")
+    url = job.find("div", class_="tooltip--flag-logo").next_sibling
+    if url:
+        url = url["href"]
+    job_data = {
+        "title": title.text,
+        "company": company.text,
+        "position": position.text,
+        "region": region.text,
+        "url": f"https://weworkremotely.com{url}",
+    }
+    all_jobs.append(job_data)
+print(all_jobs)
