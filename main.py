6f755a7dc7b4ca2a00514edae0e05499dfdb6fa0
eunhyung-lee <dldnsgud12@naver.com>
2024-09-19 16:09:44 +0900

    #6.3 Jobs

--- main.py
+++ main.py
@@ -10,5 +10,12 @@ response = requests.get(url)
 # Beautiful soup로 html parsing
 soup = BeautifulSoup(response.content, "html.parser")
 
-jobs = soup.find("section", class_="jobs").find_all("li")
-print(jobs)
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+
+for job in jobs:
+    title = job.find("span", class_="title")
+    company, position, region = job.find_all("span", class_="company")
+    company = company.text
+    position = position.text
+    region = region.text
+    print(f"{company}  {position}  {region}")
