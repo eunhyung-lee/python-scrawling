80073670c3a7519e4becb32052d3833c5059d7bd
eunhyung-lee <dldnsgud12@naver.com>
2024-09-22 00:10:26 +0900

    #6.11 Collecting Jobs

--- dynamic.py
+++ dynamic.py
@@ -7,7 +7,7 @@ browser = p.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-url = "https://www.wanted.co.kr/"
+url = "https://www.wanted.co.kr"
 
 page.goto(url)
 
@@ -27,7 +27,7 @@ time.sleep(3)
 
 page.click("a#search_tab_position")
 
-for x in range(5):
+for x in range(3):
 
     time.sleep(3)
 
@@ -40,3 +40,16 @@ full_html = page.content()
 p.stop()
 
 soup = BeautifulSoup(full_html, "html.parser")
+
+jobs = soup.find_all(
+    "div", class_="JobCard_container__REty8 JobCard_container--variant-card__gaJS_"
+)
+
+job_list = []
+for job in jobs:
+    link = f"{url}{job.find('a')['href']}"
+    title = job.find("strong", class_="JobCard_title__HBpZf").text
+    company = job.find("span", class_="JobCard_companyName__N1YrF").text
+    job_temp = {"link": link, "title": title, "company": company}
+    job_list.append(job_temp)
+    print(job_temp)
