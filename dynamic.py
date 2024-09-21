31c2b118d73b24ed219beb170228d9c07afe691a
eunhyung-lee <dldnsgud12@naver.com>
2024-09-21 23:53:23 +0900

    #6.10 Interactivity

--- dynamic.py
+++ dynamic.py
@@ -1,10 +1,42 @@
 from playwright.sync_api import sync_playwright
+import time
+from bs4 import BeautifulSoup
 
 p = sync_playwright().start()
 browser = p.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-page.goto("https://www.wanted.co.kr/search?query=python&tab=position")
+url = "https://www.wanted.co.kr/"
 
-page.screenshot(path="scrennshot.png")
+page.goto(url)
+
+time.sleep(3)
+
+page.click("button.Aside_searchButton__rajGo")
+
+time.sleep(3)
+
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+
+time.sleep(3)
+
+page.keyboard.down("Enter")
+
+time.sleep(3)
+
+page.click("a#search_tab_position")
+
+for x in range(5):
+
+    time.sleep(3)
+
+    page.keyboard.down("End")
+
+time.sleep(3)
+
+full_html = page.content()
+
+p.stop()
+
+soup = BeautifulSoup(full_html, "html.parser")
